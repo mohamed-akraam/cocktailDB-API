92a1f32ca04168ce34f882de51e3ccdf439970c8
Mohamed <7mohamedakram@gmail.com>
2022-10-30 22:10:45 +0200

    add new challenge

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+document.querySelector('button').addEventListener('click', getDrink);
+
+
+function getDrink() {
+
+    let input = document.querySelector('input').value.toLowerCase();
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+            console.log(data.drinks)
+            console.log(data.drinks.length);
+            for (let i = 0; i < data.drinks.length; i++) {
+                setTimeout(() => {
+                    document.querySelector('h2').textContent = data.drinks[i].strDrink;
+                    document.querySelector('img').src = data.drinks[i].strDrinkThumb;
+                    document.querySelector('h3').textContent = data.drinks[i].strInstructions;
+                }, i * 1000)
+            }
+    })
+    .catch(err => {
+        console.log(`error is ${err}`);
+    })
+}
